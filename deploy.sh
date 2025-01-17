3454417489b1b254561fda523789dcbffda43bbe
asmae <zaidias@grenoble-inp.org>
2025-01-17 21:16:25 +0100

    changing back

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,8 @@ terraform apply --auto-approve
 echo "Terraform apply completed."
 
 # Add a 15-second delay
-echo "Waiting for 15 seconds before starting Ansible playbook..."
-sleep 15
+echo "Waiting for 60 seconds before starting Ansible playbook..."
+sleep 60
 
 echo "Starting Ansible playbook..."
 ansible-playbook -i ../k8s_setup/inventory/hosts.yaml ../k8s_setup/playbook.yaml
