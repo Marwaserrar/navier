cb5715b2bb95a2e57610f1c1123aba2cb3fe9c2a
asmae <zaidias@grenoble-inp.org>
2025-01-17 18:22:16 +0100

    automating the flow

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e  
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+echo "Starting Terraform apply..."
+cd terraform
+terraform destroy --auto-approve
+terraform apply --auto-approve
+
+echo "Terraform apply completed."
+
+# Add a 15-second delay
+echo "Waiting for 15 seconds before starting Ansible playbook..."
+sleep 15
+
+echo "Starting Ansible playbook..."
+ansible-playbook -i ../k8s_setup/inventory/hosts.yaml ../k8s_setup/playbook.yaml
+
--- terraform/res.tf
+++ terraform/res.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("/Users/air/Documents/ensimag/google-cloud-sdk/bin/Desktop/key-file.json")
+  credentials = file("/tmp/key-file.json")
   project     = "my-first-for-kubespray"
   region      = "europe-west4"
 }
@@ -63,7 +63,7 @@ resource "google_compute_instance" "k8s_nodes" {
   }
 
   metadata = {
-    ssh-keys = "asmae:${file("/Users/air/.ssh/gcp_key.pub")}"
+    ssh-keys = "asmae:${file("/tmp/.ssh/gcp_key.pub")}"
   }
 
   service_account {
