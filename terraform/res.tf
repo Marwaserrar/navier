b471160c1ca3b5fdb20ceec5edcf2598c25e7ccb
asmae <zaidias@grenoble-inp.org>
2025-01-17 18:58:57 +0100

    checkout fixed

--- terraform/res.tf
+++ terraform/res.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"  
+         }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0" 
+    }
+  }
+}
 provider "google" {
   credentials = file("/tmp/key-file.json")
   project     = "my-first-for-kubespray"
